69a9189f3f5ea8dde8abf30ad60826867d882bc5
crrhodes <crrhodes@godaddy.com>
2014-05-21 14:25:56 -0500

    move java attribs to role

--- attributes/java.rb
+++
@@ -1,7 +0,0 @@
-override.java.jdk_version = 7
-override.java.install_flavor = 'oracle'
-override.java.accept_license_agreement = true
-override.java.oracle.accept_oracle_download_terms = true
-
-# stupid java cookbook
- override.java.java_home = '/usr/lib/jvm/java-7-oracle-amd64'
